ca37481de1f7ce8c722d6cda7f3fbaf1cabe89f2
Suhail Abood <suhail.abood@gmail.com>
2022-12-13 21:57:56 +0000

    Update hello.js

--- packages/sample/hello/hello.js
+++ packages/sample/hello/hello.js
@@ -1,5 +1,6 @@
 function main(args) {
     let name = args.name || 'stranger'
+    console.log(process.env);
     let greeting = 'Hello ' + process.env.SEND_GRID_A + '!'
     console.log(greeting)
     return {"body": process.env.SEND_GRID_A || 'Okay'}
